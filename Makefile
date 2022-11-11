0a97421116e37246dd22271a40f908a6b0ed2ff0
David Gasquez <davidgasquez@gmail.com>
2022-11-11 16:35:53 +0100

    fix: use lily

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .DEFAULT_GOAL := run
 
-VERSION=v0.5.2
+VERSION=v0.5.6
 IMAGE=europe-west1-docker.pkg.dev/protocol-labs-data/pl-data/filet
 
 build:
@@ -21,4 +21,4 @@ clean:
 	rm -rf *.car *.aria2
 
 send: push
-	gcloud --billing-project protocol-labs-data beta batch jobs submit lily-job-gcs-backfill-$(shell date +%s) --config gce_batch_job.json --location europe-north1
+	gcloud --billing-project protocol-labs-data beta batch jobs submit lily-job-gcs-backfill-snapshot-2295360-2298242-$(shell date +%s) --config gce_batch_job.json --location europe-north1
--- scripts/export.sh
+++ scripts/export.sh
@@ -38,23 +38,26 @@ echo "Walking from epoch ${FROM_EPOCH} to ${TO_EPOCH}"
 sleep 10
 
 # Run export
-archiver run --storage-path /tmp/data --ship-path "${EXPORT_DIR}" --min-height="${FROM_EPOCH}" --max-height="${TO_EPOCH}"
+# archiver run --storage-path /tmp/data --ship-path "${EXPORT_DIR}" --min-height="${FROM_EPOCH}" --max-height="${TO_EPOCH}"
 
 # Alternatively, we could run the export with lily
-# lily job run --storage=CSV walk --from "${FROM_EPOCH}" --to "${TO_EPOCH}"
-# lily job wait --id 1 && lily stop
+lily job run --storage=CSV walk --from "${FROM_EPOCH}" --to "${TO_EPOCH}"
+lily job wait --id 1 && lily stop
+
 # Check there are no errors on visor_processing_reports.csv
-# if grep -q "ERROR" /tmp/data/visor_processing_reports.csv; then
-#   echo "Errors found on visor_processing_reports!"
-#   exit 1
-# fi
+if grep -q "ERROR" /tmp/data/*visor_processing_reports.csv; then
+  echo "Errors found on visor_processing_reports!"
+  cat /tmp/data/*visor_processing_reports.csv | grep "ERROR"
+  exit 1
+fi
 
 # Compress the CSV files
-# gzip /tmp/data/*.csv
+gzip /tmp/data/*.csv
 
 # Move files to export dir
-# echo "Saving CSV files to ${EXPORT_DIR}"
-# FILENAME=$(basename "${SNAPSHOT_FILE}" .car.zst)
-# mkdir -p "$EXPORT_DIR"/"$FILENAME"/
-# mv /tmp/data/*.csv.gz "$EXPORT_DIR"/"$FILENAME"/
-# mv lily.log "$EXPORT_DIR"/"$FILENAME"/
+echo "Saving CSV files to ${EXPORT_DIR}"
+FILENAME=$(basename "${SNAPSHOT_FILE}" .car.zst)
+if [ -d "${EXPORT_DIR:?}"/"${FILENAME:?}"/ ]; then rm -Rf "${EXPORT_DIR:?}"/"${FILENAME:?}"/; fi
+mkdir -p "$EXPORT_DIR"/"$FILENAME"/
+mv /tmp/data/*.csv.gz "$EXPORT_DIR"/"$FILENAME"/
+mv lily.log "$EXPORT_DIR"/"$FILENAME"/
