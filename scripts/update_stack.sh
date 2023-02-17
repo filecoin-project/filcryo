ab3b3adf4b7b0b1dcb2f32a1ebd646d69f3c791b
Hector Sanjuan <code@hector.link>
2023-02-17 13:55:55 +0100

    Improve update_stack script

--- scripts/update_stack.sh
+++ scripts/update_stack.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+set -e
 # Pull any updates to the filcryo repository.
-pushd /opt/filcryo || exit 1
+pushd /opt/filcryo || exit 0
 
 # Fetch the latest changes from the remote repository
 git fetch
 
 # Check if there are any changes to be merged
-if git diff HEAD origin/main --quiet; then
-  echo "No changes to the repository. Exiting."
+if git diff HEAD origin/main --quiet && \
+	[ "$(docker compose -f /opt/filcryo/docker-compose.yml ps -q)" != "" ];
+then
+  echo "No changes to the repository and the Filcryo stack is running. Exiting."
   exit 0
 fi
+
+echo "Pulling repository and deploying the Filcryo stack"
 
 # Pull the latest changes from the remote repository
 git pull origin/main
